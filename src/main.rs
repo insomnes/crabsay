1989039f64965d66429d935751670370ab57d0f8
insomnes <denginm@gmail.com>
2024-07-12 17:00:16 +0200

    Add multiline support

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use std::{
     error::Error,
     io::{self, Write},
+    process::exit,
 };
 
-use crabsay::{get_input, make_boxed_text};
+use crabsay::{get_input, get_terminal_width, is_valid_width, make_boxed_text};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let line = get_input()?;
-    let boxed_text = make_boxed_text(&line);
+    let width = get_terminal_width();
+    if !is_valid_width(width) {
+        eprintln!("Terminal too narrow to display crab");
+        exit(1);
+    }
+    let lines = get_input()?;
+    if lines.is_empty() {
+        eprintln!("No input provided");
+        exit(1);
+    }
+    let boxed_text = make_boxed_text(lines, width);
     io::stdout().write_all(boxed_text.as_bytes())?;
     Ok(())
 }
